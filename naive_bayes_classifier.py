0fce9e072c5d090e1178af2d8abbce32dd15c0d0
Felix Zhang <felixfzhang@hotmail.com>
2022-07-07 22:07:51 -0400

    Implement naive Bayes classification

--- naive_bayes_classifier.py
+++ naive_bayes_classifier.py
@@ -1,16 +1,55 @@
 import numpy as np
 import tensorflow as tf
+import joblib
 
 import sklearn
+from sklearn.model_selection import StratifiedKFold
 from sklearn.naive_bayes import ComplementNB, MultinomialNB
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline, make_pipeline
 
 import skopt
 from skopt.space import Categorical, Real
 
-from preprocessing import ds_to_ndarray
 
-def train_naive_bayes(vec_ds, **params):
+from preprocessing import ds_to_ndarray, load_vec_ds, normalize_ds
+
+
+def train_nb_classifier(vec_ds: tf.data.Dataset, **params):
+    x, y = ds_to_ndarray(vec_ds)
+
+    clf = make_pipeline(
+        # MultinomialNB(alpha=0.17601196310151052)
+        MultinomialNB(alpha=0.9314472681366592)
+    )
+
+    skf = StratifiedKFold(n_splits=params['NUM_FOLDS'])
+    precision, recall, f1 = [], [], []
+
+    for i_train, i_test in skf.split(x, y):
+        print(' - Training SVM classifier on test fold')
+        with joblib.parallel_backend('threading', n_jobs=-1):
+            clf.fit(x[i_train], y[i_train])
+
+        print(' - Classifying test fold')
+        pred = clf.predict(x[i_test])
+
+        precision.append(sklearn.metrics.precision_score(y[i_test], pred))
+        recall.append(sklearn.metrics.recall_score(y[i_test], pred))
+        f1.append(sklearn.metrics.f1_score(y[i_test], pred))
+
+        print(sum(y[i_test]), sum(pred))
+        # print("Precision:", sklearn.metrics.precision_score(y[i_test], pred))
+        # print("Recall:", sklearn.metrics.recall_score(y[i_test], pred))
+
+    print('')
+    print(f'{precision} \nAverage precision: {sum(precision) / len(precision)} \n')
+    print(f'{recall} \nAverage recall: {sum(recall) / len(recall)} \n')
+    print(f'{f1} \nAverage F1 score: {sum(f1) / len(f1)}')
+
+    return clf
+
+
+def optimize_hyperparameters(vec_ds: tf.data.Dataset, **params):
     pipe = Pipeline([
         ('model', sklearn.naive_bayes.GaussianNB())
     ])
@@ -42,11 +81,6 @@ def train_naive_bayes(vec_ds, **params):
 
 if __name__ == '__main__':
     import yaml
-    import pathlib
-    import joblib
-
-    from preprocessing import import_multiple_excel, create_vectorize_layer, \
-        normalize_ds, preprocess_text_ds, vectorize_ds
 
     with open('settings.yaml', 'r') as f:
         env_vars = yaml.safe_load(f)
@@ -54,29 +88,9 @@ if __name__ == '__main__':
         settings = env_vars['SETTINGS']
         params = env_vars['PARAMETERS']
 
-    data_dir = pathlib.Path().resolve() / settings['DATA_DIR']
-    files = [data_dir / f for f in ('SALG-Instrument-78901-2.xlsx', 'SALG-Instrument-92396.xlsx')]
-
-    print('Importing raw text data and labeling')
-    text_ds, code_ds = import_multiple_excel(files, 'matter22', [(1, 1)] * 2, [(89, 15), (353, 18)], [(6, 7), (5, 6)])
-
-    print('Converting data into a numpy array')
-    # Only consider entries that have been labelled
-    mask = (code_ds != '-') & (code_ds != '+')
-    mask_ds = preprocess_text_ds(np.ma.masked_array(text_ds, mask).compressed())
-    mask_code = np.ma.masked_array(code_ds, mask).compressed()
-    mask_code = np.asarray(list(int(i == '+') for i in mask_code))
-
-    print('Converting data into vectorized Tensorflow dataset')
-    # Creates the dataset in Tensorflow
-    ds = tf.data.Dataset.from_tensor_slices((mask_ds, mask_code)).batch(params['BATCH_SIZE'], drop_remainder=True)
-    vectorize_layer = create_vectorize_layer(ds)
-
-    # Vectorize the dataset
-    vec_ds = vectorize_ds(vectorize_layer, ds, **params)[0]
+    vec_ds = load_vec_ds(settings['DATA_DIR'], **params)[0]
     norm_vec_ds = normalize_ds(vec_ds)
 
     print('Optimizing naive Bayes classifier')
-    # train_svm(vec_ds, **params)
-    opt = train_naive_bayes(vec_ds, **params)
-    joblib.dump(opt, 'models/nbc.joblib')
+
+    clf = train_nb_classifier(vec_ds, **params)
