c74f9c95976f2852e4cc5f36d0cd564e6e501e94
Felix Zhang <felixfzhang@hotmail.com>
2022-07-28 17:48:48 -0400

    Manually implement weighted Adam metric

--- bert_classifier.py
+++ bert_classifier.py
@@ -3,8 +3,8 @@ import pathlib
 import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_text as text
-from official.nlp import optimization
 
+from adamw_metric import create_optimizer
 from epoch_model_checkpoint import save_graph
 from f1_score import F1Score
 from preprocessing import load_ds, load_env_vars
@@ -160,7 +160,7 @@ def train_bert(ds_list: list[tf.data.Dataset], logs_dir: pathlib.Path, **params)
 
     net = outputs['pooled_output']
     net = tf.keras.layers.Dropout(0.1)(net)
-    net = tf.keras.layers.Dense(1, activation=None, name='classifier')(net)
+    net = tf.keras.layers.Dense(1, activation='sigmoid', name='classifier')(net)
 
     model = tf.keras.Model(text_input, net, name='bert')
 
@@ -170,27 +170,24 @@ def train_bert(ds_list: list[tf.data.Dataset], logs_dir: pathlib.Path, **params)
     num_train_steps = steps_per_epoch * params['EPOCHS']
     num_warmup_steps = int(0.1 * num_train_steps)
     init_lr = 3e-5
-    optimizer = optimization.create_optimizer(init_lr=init_lr,
-                                              num_train_steps=num_train_steps,
-                                              num_warmup_steps=num_warmup_steps,
-                                              optimizer_type='adamw')
+    optimizer = create_optimizer(init_lr=init_lr, num_train_steps=num_train_steps,
+                                 num_warmup_steps=num_warmup_steps)
 
     model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
                   optimizer=optimizer,
                   metrics=[
                       'accuracy',  # 'precision', 'recall'
-                      # tf.keras.metrics.Precision(name='precision'),
-                      # tf.keras.metrics.Recall(name='recall'),
-                      # F1Score(name='f1'),
+                      tf.keras.metrics.Precision(name='precision'),
+                      tf.keras.metrics.Recall(name='recall'),
+                      F1Score(name='f1'),
                   ])
 
     history = model.fit(x=ds_list[0], validation_data=ds_list[1], epochs=params['EPOCHS'])
 
     metrics = model.evaluate(ds_list[2])
 
-    # f1 = 0 if metrics[2] * metrics[3] == 0 else (2 * metrics[2] * metrics[3]) / (metrics[2] + metrics[3])
-    # save_graph(logs_dir / 'graphs' / f'{f1}.png', history)
-    save_graph(logs_dir / 'graphs' / 'bert.png', history)
+    f1 = 0 if metrics[2] * metrics[3] == 0 else (2 * metrics[2] * metrics[3]) / (metrics[2] + metrics[3])
+    save_graph(logs_dir / 'graphs' / f'bert-{f1}.png', history)
 
     return model, history
 
@@ -207,6 +204,6 @@ if __name__ == '__main__':
     ds_list = load_ds(settings['BASE_DIR'] / settings['DATA_DIR'], is_xlsx=False, prefix='sen_', **params)
     options = tf.data.Options()
     options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.DATA
-    ds_list = [ds.with_options(options).batch(params['BATCH_SIZE']).cache().prefetch(tf.data.AUTOTUNE) for ds in ds_list]
+    ds_list = [ds.with_options(options).cache().prefetch(tf.data.AUTOTUNE) for ds in ds_list]
 
     train_bert(ds_list, settings['BASE_DIR'] / settings['LOGS_DIR'], **params)
